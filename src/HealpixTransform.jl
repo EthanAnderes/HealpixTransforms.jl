354adb47ea7b6d9b58142ac46e39707247baf283
Ethan Anderes <anderes@ucdavis.edu>
2020-06-15 15:56:16 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.4

---
+++ src/HealpixTransform.jl
@@ -0,0 +1,5 @@
+module HealpixTransform
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HealpixTransform
+using Test
+
+@testset "HealpixTransform.jl" begin
+    # Write your tests here.
+end
